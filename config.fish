c121fadd74518ffd2bf04f478ef1e33fef006213
ysks <clad.zero@gmail.com>
2026-09-23 21:56:13 +0900

    chore(fish): config.fish をホームの最新設定に同期
    - peco から fzf への移行、zoxide・pyenv・PATH 設定などを反映
    - ホーム側でのみ更新されていた内容を取り込む

--- config.fish
+++ config.fish
@@ -5,99 +5,102 @@ alias vi='/usr/bin/vim'
 abbr -a vc vim ~/.config/fish/config.fish
 abbr -a sc source ~/.config/fish/config.fish
 
-## How to install EZA -> https://github.com/eza-community/eza/blob/main/INSTALL.md
-if command -q eza
-	abbr -a ls eza --icons
-    abbr -a ll eza --icons -lhg --time-style long-iso
-    abbr -a la eza --icons -lhag --time-style long-iso
-    abbr -a lt eza --icons --tree
-end
-
-## Useful common command snippets
 abbr -a head head -n5
 abbr -a tail tail -n5
 abbr -a c clear
-abbr -a t htop  # htop is a modern replacement of top
+abbr -a t htop
 abbr -a wc wc -l
 abbr -a h "history | grep"
+
 abbr -a sed --set-cursor "sed 's/%//g'"
 abbr -a awk --set-cursor "awk -F, '{print \$1%}'"
 abbr -a cnt "awk -F, '{cnt[\$1]++}END{for(i in cnt)print i,cnt[i]}'"
 abbr -a unq "awk -F, '{cnt[\$1]++}END{for(i in cnt)print i}'"
 abbr -a header --set-cursor "head -n1 % | awk -F, '{for(i=1;i<=NF;i++)print i,\$i}' | column -t"
-## Useful git command snippets
+
+if command -q eza
+    abbr -a ls eza --icons
+    abbr -a ll eza --icons -lhg --time-style long-iso
+    abbr -a la eza --icons -lhag --time-style long-iso
+    abbr -a lt eza --icons --tree
+end
+
+## app alias
+# claude CLI shortcuts
+abbr -a cc 'claude -c'
+abbr -a cr 'claude -r'
+alias cot='open -a coteditor'
+alias python='python3'
+alias pip='uv pip'
+abbr -a p python
+alias bu='brew upgrade'
+alias bcs='brew cleanup -s'
+function purge
+    sudo purge
+end
+alias mem='top -l 1 -s 0 | grep PhysMem'
+alias weather="curl 'wttr.in/Tokyo?lang=ja'"
+
+# git alias
 alias gb='git branch'
 alias gba='git branch -a'
 alias gbd='git branch -D'
+alias gbm="git branch --merged | grep -vE '^\*|main|master|develop'"
+alias gbdm="git branch --merged | grep -vE '^\*|main|master|develop' | xargs -r git branch -d"
 alias gcm='git commit -m'
 alias gco='git checkout'
 alias gcob='git checkout -b'
 alias gd='git diff'
+alias gpull='git pull'
+alias gpush='git push'
 alias glog="git log --graph --date=iso --pretty='[%ad]%C(auto) %h%d %Cgreen%an%Creset : %s'"
+function gmain
+    git checkout main
+    and git pull
+    and gbdm
+end
 alias gll="git log --pretty=format:'%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]' --decorate --numstat"
 
-alias pip='uv pip'
+command -q gh; and gh completion -s fish | source
 
-# function fish_user_key_bindings
-#   bind \cr 'peco_select_history (commandline -b)' # Bind for prco history to Ctrl+r
-#   bind \c] peco_change_directory # Bind for prco change directory to Ctrl+]
-# end
-
-# function cd
-#     if test (count $argv) -eq 0
-#         return 0
-#     else if test (count $argv) -gt 1
-#         printf "%s\n" (_ "Too many args for cd command")
-#         return 1
-#     end
-#     # Avoid set completions.
-#     set -l previous $PWD
-
-#     if test "$argv" = "-"
-#         if test "$__fish_cd_direction" = "next"
-#             nextd
-#         else
-#             prevd
-#         end
-#         return $status
-#     end
-#     builtin cd $argv
-#     set -l cd_status $status
-#     # Log history
-#     if test $cd_status -eq 0 -a "$PWD" != "$previous"
-#         set -q dirprev[$MAX_DIR_HIST]
-#         and set -e dirprev[1]
-#         set -g dirprev $dirprev $previous
-#         set -e dirnext
-#         set -g __fish_cd_direction prev
-#     end
-
-#     if test $cd_status -ne 0
-#         return 1
-#     end
-#     ls
-#     return $status
-# end
-
-## For TypeScript/JavaScript PJ
 alias nrb='npm run build'
 alias nrd='npm run dev'
 
-## npx (modify to use bun)
+set -g fish_greeting
+set -gx HOMEBREW_NO_ENV_HINTS TRUE
+
+# Go
+set -gx GOPATH $HOME/go
+
+# AWS
+set -gx AWS_DEFAULT_PROFILE dev
+
+# pyenv
+set -gx PYENV_ROOT $HOME/.pyenv
+fish_add_path $PYENV_ROOT/bin
+pyenv init - | source
+
+# fzf (peco から移行)
+set -gx GHQ_SELECTOR fzf
+
+set -gx LDFLAGS "-L/opt/homebrew/opt/icu4c/lib -L/opt/homebrew/opt/openssl@3/lib -L/opt/homebrew/opt/readline/lib"
+set -gx CPPFLAGS "-I/opt/homebrew/opt/icu4c/include -I/opt/homebrew/opt/openssl@3/include -I/opt/homebrew/opt/readline/include"
+set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/icu4c/lib/pkgconfig:/opt/homebrew/opt/openssl@3/lib/pkgconfig:/opt/homebrew/opt/readline/lib/pkgconfig"
+
+fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/sbin
+fish_add_path $GOPATH/bin
+fish_add_path /opt/homebrew/opt/openssl@3/bin
+fish_add_path ~/.local/bin
+
+# npx -> bun
 alias ccusage="bunx --bun ccusage@latest"
 alias ncu="bunx --bun npm-check-updates"
 alias npx="bunx --bun"
-alias difit="bunx --bun difit (git log --oneline --decorate | peco --prompt 'COMMIT >' | string split ' ' --)[1]"
+function difit
+    bunx --bun difit (git log --oneline --decorate | fzf --prompt 'COMMIT > ' | string split ' ' --)[1]
+end
 
+fzf --fish | source
+zoxide init fish | source
 
-##for Mac
-# alias bu='brew upgrade'
-# alias bcs='brew cleanup -s'
-# function purge
-	#sudo purge
-	#rm -rf /Users/yusuke/Library/Caches/Google/
-# 	du -sx / &> /dev/null &; sleep 5; kill $last_pid
-# end
-# alias mem='top -l 1 -s 0 | grep PhysMem'
-## rmでゴミ箱に入れる
-#alias rm='rmtrash'
